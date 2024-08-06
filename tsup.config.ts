85b84fc34193a10402c9429973aef46cf28ab255
uzmoi <rizzzse@gmail.com>
2024-08-06 21:52:07 +0900

    package settings

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,15 @@ import { defineConfig } from "tsup";
 export default defineConfig([
   {
     entry: ["src/index.ts"],
-    format: "esm",
+    format: ["esm", "cjs"],
+    sourcemap: true,
     dts: true,
+    clean: true,
   },
   {
     entry: ["src/processors/*.ts"],
     outDir: "dist/processors",
     target: "esnext",
+    splitting: true,
   },
 ]);
